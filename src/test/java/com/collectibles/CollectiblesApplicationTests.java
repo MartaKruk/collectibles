87b33572932619cc0bfcd371adabe1bc336f0a27
Marta Kruk <kruk.marta@outlook.com>
2022-03-13 17:47:07 +0100

    First commit

---
+++ src/test/java/com/collectibles/CollectiblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectibles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectiblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
