aa8dfa21b5fa931561332ce24d4bf5e68c87b95d
Marta Kruk <kruk.marta@outlook.com>
2022-04-05 22:44:02 +0200

    added swagger

--- src/main/java/com/collectibles/config/CoreConfiguration.java
+++ src/main/java/com/collectibles/config/CoreConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableScheduling
 @Configuration
@@ -13,4 +17,12 @@ public class CoreConfiguration {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build(); }
 }
