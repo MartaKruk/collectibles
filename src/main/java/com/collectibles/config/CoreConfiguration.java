c5b2e02377817cc11420eeacad8def0c4d0c0cc4
Marta Kruk <kruk.marta@outlook.com>
2022-04-06 10:44:12 +0200

    fixed swagger issues

--- src/main/java/com/collectibles/config/CoreConfiguration.java
+++ src/main/java/com/collectibles/config/CoreConfiguration.java
@@ -8,9 +8,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableScheduling
 @Configuration
+@EnableSwagger2
 public class CoreConfiguration {
 
     @Bean
@@ -24,5 +26,6 @@ public class CoreConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build(); }
+                .build();
+    }
 }
